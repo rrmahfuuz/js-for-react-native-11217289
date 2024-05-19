dc4ded5c8dad32bb825ab27cb141639a3f14e3c4
rrmahfuuz <msmustapha@st.ug.edu.gh>
2024-05-19 20:17:55 +0000

    task 1

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,1 +1,19 @@
-start
+function processArray(array) {
+    // Check if the input is an array
+    if (!Array.isArray(array)) {
+        throw new TypeError('Input should be an array of numbers');
+    }
+
+    // Create a new array with the processed numbers
+    return array.map(num => {
+        if (typeof num !== 'number') {
+            throw new TypeError('All elements in the array should be numbers');
+        }
+        return (num % 2 === 0) ? num * num : num * 3;
+    });
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const processedNumbers = processArray(numbers);
+console.log(processedNumbers); // Output: [3, 4, 9, 16, 15, 36]
+module.exports = { processArray };
